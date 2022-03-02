1502204bee4c1eb4964897d71377ea4b12b82bcd
intrnl <intrnl28+git@gmail.com>
2022-03-02 20:40:37 +0700

    chore: reflect

--- improved-bd.js
+++ improved-bd.js
@@ -22,103 +22,114 @@ const STATE_NORMAL = 0; // consume `emote`
 const STATE_OVERRIDE = 1; // consume either `override` or `modifier`
 const STATE_MODIFIER = 2; // consume `modifier`
 
-export function search (string) {
-	const result = [];
+export function search (nodes) {
+	for (let n = 0; n < nodes.length; n++) {
+		const node = nodes[n];
 
-	let idx = 0;
-	let len = string.length;
+		if (typeof node !== 'string') {
+			continue;
+		}
 
-	while (idx < len) {
-		const start_idx = idx;
-		let state = STATE_NORMAL;
-
-		let name = '';
-		let override = '';
-		let modifier = '';
-
-		let mismatch = false;
-
+		let idx = 0;
+		let len = node.length;
+	
 		while (idx < len) {
-			const char = string[idx];
-
-			if (isWhitespace(char)) {
-				break;
+			const start_idx = idx;
+			let state = STATE_NORMAL;
+	
+			let name = '';
+			let override = '';
+			let modifier = '';
+	
+			let mismatch = false;
+	
+			while (idx < len) {
+				const char = node[idx];
+	
+				if (isWhitespace(char)) {
+					break;
+				}
+	
+				if (char === ':') {
+					// Kappa:foo:bar:baz
+					//              ^
+					if (state >= STATE_MODIFIER) {
+						mismatch = true;
+						break;
+					}
+	
+					state++;
+					idx++;
+					continue;
+				}
+	
+				if (state === STATE_NORMAL) {
+					name += char;
+				}
+				else if (state === STATE_OVERRIDE) {
+					override += char;
+				}
+				else if (state === STATE_MODIFIER) {
+					modifier += char;
+				}
+	
+				idx++;
 			}
-
-			if (char === ':') {
-				// Kappa:foo:bar:baz
-				//              ^
+	
+			if (mismatch) {
+				// eat through non-whitespace
+				while (idx < len && !isWhitespace(node[idx])) {
+					idx++;
+				}
+			}
+	
+			// eat through whitespace
+			while (idx < len && isWhitespace(node[idx])) {
+				idx++;
+			}
+	
+			if (mismatch || name.length < 4) {
+				continue;
+			}
+	
+			if (!override || !(override in Emotes)) {
+				modifier = override;
+	
+				// go through each source and see if we can find a match
+				for (let source in Emotes) {
+					const emotes = Emotes[source];
+	
+					if (name in emotes) {
+						override = source;
+						break;
+					}
+				}
+	
+				if (!override) {
+					continue;
+				}
+			} else if (!(name in Emotes[override])) {
+				continue;
+			}
+	
+			if (!modifier || !modifiers.includes(modifier)) {
 				if (state >= STATE_MODIFIER) {
-					mismatch = true;
-					break;
+					continue;
 				}
-
-				state++;
-				idx++;
-				continue;
+	
+				modifier = null;
 			}
 
-			if (state === STATE_NORMAL) {
-				name += char;
-			}
-			else if (state === STATE_OVERRIDE) {
-				override += char;
-			}
-			else if (state === STATE_MODIFIER) {
-				modifier += char;
-			}
+			const pre = node.substring(0, start_idx);
+			const post = node.substring(idx);
 
-			idx++;
+			const obj = { type: 'emote', category: override, name, modifier };
+
+			nodes[n] = pre;
+			nodes.splice(n + 1, 0, obj, post);
+			break;
 		}
-
-		if (mismatch) {
-			// eat through non-whitespace
-			while (idx < len && !isWhitespace(string[idx])) {
-				idx++;
-			}
-		}
-
-		// eat through whitespace
-		while (idx < len && isWhitespace(string[idx])) {
-			idx++;
-		}
-
-		if (mismatch || name.length < 4) {
-			continue;
-		}
-
-		if (!override || !(override in Emotes)) {
-			modifier = override;
-
-			// go through each source and see if we can find a match
-			for (let source in Emotes) {
-				const emotes = Emotes[source];
-
-				if (name in emotes) {
-					override = source;
-					break;
-				}
-			}
-
-			if (!override) {
-				continue;
-			}
-		} else if (!(name in Emotes[override])) {
-			continue;
-		}
-
-		if (!modifier || !modifiers.includes(modifier)) {
-			if (state >= STATE_MODIFIER) {
-				continue;
-			}
-
-			modifier = null;
-		}
-
-		result.push([start_idx, override, name, modifier]);
 	}
-
-	return result;
 }
 
 function isWhitespace (char) {
