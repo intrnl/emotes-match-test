fa7270142d251fd47f942cde23092425ab3bd36f
intrnl <intrnl28+git@gmail.com>
2022-03-02 20:50:30 +0700

    chore: perform 10000 iterations

--- improved-bd-test.js
+++ improved-bd-test.js
@@ -4,11 +4,13 @@ const mem_end = process.memoryUsage().heapUsed;
 
 console.log(`${(mem_end - mem_start) / 1024 / 1024} MB used`);
 
-const nodes = ['foKappa    a quick brown fox jumps over the lazy     PixelBob:spin dog      Kappa'];
-
 const match_start = performance.now();
-const result = search(nodes);
+
+for (let idx = 0; idx < 10_000; idx++) {
+	const nodes = ['Kappa Kappa foKappa    a quick brown fox jumps over the lazy     PixelBob:spin dog      Kappa'];
+	search(nodes);
+}
+
 const match_end = performance.now();
 
-console.log(`match took ${match_end - match_start} ms`);
-console.log(nodes);
+console.log(`took ${match_end - match_start} ms`);
